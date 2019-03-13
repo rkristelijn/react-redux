2cd9b66dd3d9ba6e7901960f87910ef1fa27fa57
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-13 15:56:37 +0100

    feat | first comp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 
+import CreateClass from "./CreateClass";
+
 function Hi() {
   debugger;
-  return <p>Hi.</p>;
+  return (
+    <div>
+      <p>Hi.</p>
+      <CreateClass />
+    </div>
+  );
 }
 
 render(<Hi />, document.getElementById("app"));
