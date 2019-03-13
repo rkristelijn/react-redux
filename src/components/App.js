ba83128b60e6a0dece48d10fe008c6ce170d06ce
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-13 17:11:22 +0100

    feat | page not found

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import Aboutpage from "./about/AboutPage";
 import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={Aboutpage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={Aboutpage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PageNotFound = () => <h1>This is not the page you are looking for</h1>;
+
+export default PageNotFound;
