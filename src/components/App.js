ac2d057523f3e684a17dec3eb4af7e2b760a8981
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-20 13:34:22 +0100

    feat | working route for manage page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
-import Aboutpage from "./about/AboutPage";
+import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursePage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={Aboutpage} />
+        <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CoursesPage} />
+        <Route path="/course:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
