495298ae2460a9c05a437ddbfc4577d7de9ad419
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-20 13:56:01 +0100

    feat | use hooks, but runs always onLoad

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 
-class CoursesPage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+function CoursesPage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => {
         alert("Loading courses failed: " + error);
@@ -17,15 +16,13 @@ class CoursesPage extends React.Component {
         alert("Loading authors failed: " + error);
       });
     }
-  }
-  render() {
-    console.log("ManageCoursePage", "render", this.props);
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  });
+
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 CoursesPage.propTypes = {
