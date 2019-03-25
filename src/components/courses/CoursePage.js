66de2b7f094eb9c12dc6216a33a34affe6f13abd
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-25 11:08:23 +0100

    feat | always show spinner

--- src/components/courses/CoursePage.js
+++ src/components/courses/CoursePage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
 
 // Separation of concerns:
 //   CoursePage should be a container component, it is now displaying and adding courses
@@ -34,6 +35,7 @@ class CoursesPage extends React.Component {
       <>
         {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
+        <Spinner />
         <button
           style={{ marginBottom: 20 }}
           className="btn btn-primary add-course"
