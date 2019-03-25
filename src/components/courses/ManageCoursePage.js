80d5f87e07769b3fb7d75422eb893e8c3ae4e1b3
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-25 16:54:43 +0100

    feat | client side validation

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -41,6 +41,18 @@ function ManageCoursePage({
     }
   }, [props.course]);
 
+  function formIsValid() {
+    const { title, authorId, category } = course;
+    const errors = {};
+    if (!title) errors.title = "Title is required.";
+    if (!authorId) errors.author = "Author is required.";
+    if (!category) errors.category = "Category is required.";
+
+    setErrors(errors);
+    //Form is valid if the errors object still has no properties
+    return Object.keys(errors).length === 0;
+  }
+
   function handleChange(event) {
     const { name, value } = event.target;
     console.log("ManageCoursesPage", "handleChange", name, value);
@@ -53,6 +65,11 @@ function ManageCoursePage({
   function handleSave(event) {
     console.log("ManageCoursesPage", "handleSave", course);
     event.preventDefault();
+
+    if (!formIsValid()) {
+      return;
+    }
+
     setSaving(true);
     saveCourse(course)
       .then(() => {
