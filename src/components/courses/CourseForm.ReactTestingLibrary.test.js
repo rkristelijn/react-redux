e9813b050c8ba3e2bd2786d6a5017bd3ddab9375
Remi Kristelijn <remi.kristelijn@accenture.com>
2019-03-26 13:57:46 +0100

    feat | React Testing Library cleanup

--- src/components/courses/CourseForm.ReactTestingLibrary.test.js
+++ src/components/courses/CourseForm.ReactTestingLibrary.test.js
@@ -30,7 +30,6 @@ it("should label save button as 'Save' when not saving", () => {
 });
 
 it("should label save button as 'Save' when not saving", () => {
-  const { getByText, debug } = renderCourseForm({ saving: true });
-  debug();
+  const { getByText } = renderCourseForm({ saving: true });
   getByText("Saving..."); //assertion build in
 });
